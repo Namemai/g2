eba7decef983ca0d1e2bcc00240c3466faf7cc62
Namemai <40319864+Namemai@users.noreply.github.com>
2020-03-21 09:35:59 +0700

    Update __init__.py

--- KIA/__init__.py
+++ KIA/__init__.py
@@ -3,10 +3,10 @@ from .channel import LineChannel
 from .poll import LinePoll
 from akad.ttypes import OpType
 
-__copyright__       = 'Copyright 2017 by mai Rachman'
+__copyright__       = 'Copyright 2020 by mai'
 __version__         = '2.0.2'
 __license__         = 'BSD-3-Clause'
-__author__          = 'Fadhiil Rachman'
+__author__          = 'mai'
 __author_email__    = '06555mai@gmail.com'
 __url__             = 'http://github.com/fadhiilrachman/line-py'
 
