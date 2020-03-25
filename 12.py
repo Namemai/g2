51927f55352ec28ab8a145b0b4941664b982e236
Namemai <40319864+Namemai@users.noreply.github.com>
2020-03-26 00:07:37 +0700

    Update 12.py

--- 12.py
+++ 12.py
@@ -2737,7 +2737,7 @@ def bot(op):
 
         if op.type == 55:
             if op.param2 not in Bots and op.param2 not in owner and op.param2 not in admin and op.param2 not in staff:
-                wait["blacklist"][op.param2]:
+                wait["blacklist"][op.param2] = True
                 try:
                     ki.cancelGroupInvitation(op.param1,[op.param3])
                     ki.kickoutFromGroup(op.param1,[op.param2])
